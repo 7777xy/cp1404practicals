7c9495abad4d4aa5224cb283c0daf0d44c312786
lixingye <2890794498@qq.com>
2024-06-22 23:06:50 +0800

    practical 5

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,45 +6,50 @@ Actual:  45 minutes
 
 import csv
 
+FILENAME = "wimbledon.csv"
+
 
 def main():
     """print the champions and the winning countries from the file."""
-    filename = "wimbledon.csv"
     name_to_number = {}
+    data = []
     countries = set()
-    with open(filename, "r", encoding="utf-8-sig") as in_file:
-        text = csv.reader(in_file)
-        next(text)
-        process_data(name_to_number, text, countries)
-        print_champion(name_to_number)
-        print()
-        print_winning_country(countries)
-
-
-def process_data(name_to_number, text, countries):
-    """Calculate the winning times and collect winning countries from the file."""
-    for record in text:
-        if record[2] in name_to_number:
-            name_to_number[record[2]] += 1
-        else:
-            name_to_number[record[2]] = 1
-        countries.add(record[1])
-
-
-def print_champion(name_to_number):
-    """Print the names and times of champions."""
+    get_data(data)
+    calculate_winning_time(name_to_number, data)
     print("Wimbledon Champions: ")
     for name, number in name_to_number.items():
         print(f"{name} {number}")
-
-
-def print_winning_country(countries):
-    """Print countries that won the Wimbledon."""
-    countries = sorted(countries)
-    length = len(countries)
-    country = ', '.join(countries)
-    print(f"These {length} countries have won Wimbledon: ")
+    print()
+    country = collect_winning_country(countries, data)
+    print(f"These {len(countries)} countries have won Wimbledon: ")
     print(country)
+
+
+def get_data(data):
+    """Get data from the csv file."""
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        text = csv.reader(in_file)
+        next(text)
+        for record in text:
+            data.append(record)
+
+
+def collect_winning_country(countries, data):
+    """Collect winning countries."""
+    for part in data:
+        countries.add(part[1])
+    countries = sorted(countries)
+    country = ', '.join(countries)
+    return country
+
+
+def calculate_winning_time(name_to_number, data):
+    """Calculate winning times."""
+    for part in data:
+        if part[2] in name_to_number:
+            name_to_number[part[2]] += 1
+        else:
+            name_to_number[part[2]] = 1
 
 
 main()
