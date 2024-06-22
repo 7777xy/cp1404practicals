7d6d2d5787a4eac0016bd79650e6083988c1ead8
lixingye <2890794498@qq.com>
2024-06-22 22:07:52 +0800

    practical 5

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,19 +6,16 @@ File needs reformatting
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 
-valid_input = False
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 max_length = max([len(code) for code in CODE_TO_NAME.keys()])
 for code, name in CODE_TO_NAME.items():
     print(f"{code:{max_length}} is {name}")
-while not valid_input:
+state_code = input("Enter short state: ").upper()
+while state_code != "":
     try:
-        state_code = input("Enter short state: ").upper()
-        while state_code != "":
-            print(state_code, "is", CODE_TO_NAME[state_code])
-            state_code = input("Enter short state: ").upper()
-        valid_input = True
+        print(state_code, "is", CODE_TO_NAME[state_code])
     except KeyError:
         print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
