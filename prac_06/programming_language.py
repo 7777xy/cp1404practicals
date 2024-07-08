9f0b791d9339cc4aa8de67b6bac820bb125fc869
lixingye <2890794498@qq.com>
2024-07-09 00:35:32 +0800

    Initialise a programming language instance

---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+class ProgrammingLanguage:
+    """Represent a Programming Language object."""
+    def __init__(self, name="", typing="", reflection="", year=0):
+        """Initialise a programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
