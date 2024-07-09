733ab4353ca6aa66d0b13f2852286269587a5a69
lixingye <2890794498@qq.com>
2024-07-09 12:47:40 +0800

    Display the dynamically typed language

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,10 +1,16 @@
+"""
+Estimated duration time: 30 min
+Current time: 12:10
+Finished time: 12:50
+Actual duration time: 40 min
+"""
 from prac_06.programming_language import ProgrammingLanguage
 
 languages = []
 
 
 def main():
-    """Display the dynamically typed language."""
+    """Display language according to the class Programming Language."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
@@ -12,7 +18,15 @@ def main():
     languages.append(python)
     languages.append(ruby)
     languages.append(visual_basic)
+    display_language()
 
+
+def display_language():
+    """Display the dynamically typed language."""
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.__is_dynamic__():
+            print(language.name)
 
 
 main()
