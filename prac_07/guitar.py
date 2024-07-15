024c9f7598d434a4c06229ee75173d894394b1d1
lixingye <2890794498@qq.com>
2024-07-15 23:04:51 +0800

    Display the details of guitar

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -6,3 +6,8 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Display the details of guitar."""
+        return f"{self.name},{self.year},{self.cost:.2f}"
+
