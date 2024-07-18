7f0f426bca20f9d52c6ce3c2c689383eba77fdf0
lixingye <2890794498@qq.com>
2024-07-18 16:46:08 +0800

    Display completed projects and incomplete projects

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,13 +5,48 @@ Actual time:
 
 from prac_07.project import Project
 
+
 FILENAME = "projects.txt"
+MENU = ("- (L)oad project\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n"
+        "- (U)pdate project\n- (Q)uit")
 
 
 def main():
     """Play the program according to the menu."""
     data = []
     load_data(data)
+    print("Welcome to Pythonic Project Management")
+    print(f"Loaded {len(data)} projects from {FILENAME}")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "D":
+            display_projects(data)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+def display_projects(data):
+    """Display completed projects and incomplete projects."""
+    incomplete_projects = []
+    completed_projects = []
+    for project in data:
+        if project.__is_completed__():
+            completed_projects.append(project)
+        else:
+            incomplete_projects.append(project)
+    display_partial_projects("Incomplete projects:", incomplete_projects)
+    display_partial_projects("Completed projects:", completed_projects)
+
+
+def display_partial_projects(message, partial_projects):
+    """Display partial projects."""
+    print(message)
+    partial_projects.sort()
+    for project in partial_projects:
+        print(f" {project}")
 
 
 def load_data(data):
