031fef1e4efba9d784a1ad94c6e44046773b4cc0
lixingye <2890794498@qq.com>
2024-07-25 16:10:17 +0800

    Handle greeting and output result to label widget

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -13,5 +13,9 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        """Handle greeting, output result to label widget."""
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
 
 BoxLayoutDemo().run()
