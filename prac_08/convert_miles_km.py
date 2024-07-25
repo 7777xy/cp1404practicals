feb31d363e9d314935f563014f0e073410287848
lixingye <2890794498@qq.com>
2024-07-25 17:02:07 +0800

    Build the Kivy app from the kv file

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.properties import StringProperty
 
 MILE_TO_KM = 1.60934
@@ -7,6 +8,13 @@ MILE_TO_KM = 1.60934
 class MilesConversion(App):
     """MilesConversion is a Kivy App for converting miles to kilometres."""
     message = StringProperty()
+
+    def build(self):
+        """Build the Kivy app from the kv file."""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Type in the field & press convert"
+        return self.root
 
 
 MilesConversion().run()
