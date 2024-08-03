7a4fea3adfe81497a9604b427585a13c5870de12
lixingye <2890794498@qq.com>
2024-08-03 23:12:08 +0800

    Change the name of variable and constant

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,10 +1,11 @@
 from prac_09.unreliable_car import UnreliableCar
 
-LOWEST_DRIVING_DISTANCE = 0
+LOWEST_DISTANCE_DRIVEN = 0
 
 unreliable_car = UnreliableCar(name="Audi", fuel=10, reliability=50)
-driving_distance = unreliable_car.drive(20)
-if driving_distance != LOWEST_DRIVING_DISTANCE:
+distance_driven = unreliable_car.drive(20)
+if distance_driven != LOWEST_DISTANCE_DRIVEN:
     print("The car drives.")
 else:
     print("The car does not drive.")
+
