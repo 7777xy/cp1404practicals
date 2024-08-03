2d48a0e6a60bb078413e63ea2163ad4053b2e9e7
lixingye <2890794498@qq.com>
2024-08-03 23:19:40 +0800

    Initialize a Silver Service Taxi object

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,3 +5,8 @@ class SilverServiceTaxi(Taxi):
     """Represent a Silver Service Taxi object, based on parent class Taxi."""
     flagfall = 4.50
 
+    def __init__(self, name="", fuel=0, fanciness=0):
+        """Initialize a Silver Service Taxi object."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * self.fanciness
