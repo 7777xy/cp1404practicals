74b7be24f228b182e08efb772c82e3f6c19af015
lixingye <2890794498@qq.com>
2024-08-03 23:20:10 +0800

    Get fare like parent Taxi, but add the flagfall in the answer

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -10,3 +10,7 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * self.fanciness
+
+    def get_fare(self):
+        """Get fare like parent Taxi, but add the flagfall in the answer."""
+        return super().get_fare() + self.flagfall
