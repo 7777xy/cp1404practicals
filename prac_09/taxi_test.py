e629f9df847246acf16091ad174b323979baf420
lixingye <2890794498@qq.com>
2024-08-03 22:29:06 +0800

    Display the restarted taxi object's details and current fee

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,3 +4,8 @@ my_taxi = Taxi(name="Prius 1", fuel=100)
 my_taxi.drive(40)
 print(my_taxi)
 print(my_taxi.get_fare())
+
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
+print(my_taxi.get_fare())
