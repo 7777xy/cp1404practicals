850e2bff682c3bae3a68ed887badd1e56193c9f6
lixingye <2890794498@qq.com>
2024-08-03 23:59:11 +0800

    Choose the taxi from the given taxis

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -17,7 +17,7 @@ def main():
     choice = input(">>> ").lower()
     while choice != 'q':
         if choice == 'c':
-
+            current_taxi = get_current_taxi(current_taxi, taxis)
         elif choice == 'd':
 
         else:
@@ -26,6 +26,28 @@ def main():
         print(f"Bill to date: ${bill:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
+
+
+
+def get_current_taxi(current_taxi, taxis):
+    """Choose the taxi from the given taxis."""
+    display_taxi_detail("Taxis available:", taxis)
+    try:
+        current_taxi = int(input("Choose taxi: "))
+        if current_taxi < LOWEST_TAXI_SEQUENCE_NUMBER or current_taxi > len(taxis) - 1:
+            print("Invalid taxi choice")
+            current_taxi = None
+    except ValueError:
+        print("It should be an integer.")
+    return current_taxi
+
+
+
+def display_taxi_detail(message, taxis):
+    """Display details of each taxi."""
+    print(message)
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
 
 
 main()
